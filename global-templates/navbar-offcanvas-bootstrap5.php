f13a123cce9b618d6ba7a52bbdd7b7e5f8c58a84
Spiczak-Brzeziński <spitom>
2026-05-20 12:08:01 +0200

    Built offcanvas menu part 2

--- global-templates/navbar-offcanvas-bootstrap5.php
+++ global-templates/navbar-offcanvas-bootstrap5.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Header Navbar (bootstrap5)
- *
- * @package Understrap
- * @since 1.1.0
- */
-
-// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
 $container = get_theme_mod( 'understrap_container_type' );
@@ -18,10 +10,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 	</h2>
 
-
 	<div class="<?php echo esc_attr( $container ); ?>">
 
-		<!-- Your site branding in the menu -->
 		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
 
 		<button
@@ -40,14 +30,13 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<div class="offcanvas-header justify-content-end">
 				<button
-					class="btn-close btn-close-white text-reset"
+					class="btn-close btn-close-white"
 					type="button"
 					data-bs-dismiss="offcanvas"
 					aria-label="<?php esc_attr_e( 'Close menu', 'understrap' ); ?>"
 				></button>
-			</div><!-- .offcancas-header -->
+			</div>
 
-			<!-- The WordPress Menu goes here -->
 			<?php
 			wp_nav_menu(
 				array(
@@ -55,16 +44,15 @@ $container = get_theme_mod( 'understrap_container_type' );
 					'container'       => 'div',
 					'container_class' => 'offcanvas-body etos-navbar__offcanvas-body',
 					'container_id'    => 'navbarNavOffcanvasBody',
-					'menu_class'      => 'navbar-nav etos-navbar__menu ms-auto',
-					'fallback_cb'     => 'false',
+					'menu_class'      => 'navbar-nav etos-navbar__menu justify-content-end flex-grow-1 pe-3',
+					'fallback_cb'     => false,
 					'menu_id'         => 'main-menu',
 					'depth'           => 2,
 					'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 				)
 			);
 			?>
-		</div><!-- .offcanvas -->
 
-	</div><!-- .container(-fluid) -->
-
-</nav><!-- #main-nav -->
+		</div>
+	</div>
+</nav>
